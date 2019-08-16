03d60abf907ab532d82bbe500fad73cb2074f9ea
Eli Selkin <eli.j.selkin@gmail.com>
2019-08-16 14:20:07 -0700

    working, now pdf

--- simple_latex/simple_latex_document.py
+++ simple_latex/simple_latex_document.py
@@ -3,12 +3,13 @@ import subprocess
 from .latex_document import Document
 from .latex_preamble import Preamble
 
+
 class SimpleLatexDocument:
     def __init__(self, preamble=None, document=None, special=None):
         self.preamble = preamble
         self.document = document
         self.special = special
-    
+
     def add(self, environment):
         if isinstance(environment, Preamble):
             self.preamble = environment
@@ -26,25 +27,25 @@ class SimpleLatexDocument:
             repr += str(self.special)
         return repr
 
-    def pdf(self, directory_for_conversion, file_name_output, clean_output_directory = True):
-        latex_file_path = path.join(directory_for_conversion, file_name_output)
-        if not os.path.exists(directory_for_conversion):
-            os.makedirs(directory_for_conversion)
+    def pdf(self, directory_for_conversion, file_name_output, clean_output_directory=True):
         try:
+            latex_file_path = path.join(
+                directory_for_conversion, file_name_output)
+            if not os.path.exists(directory_for_conversion):
+                os.makedirs(directory_for_conversion)
             with open(latex_file_path, 'w') as latex_outfp:
                 latex_outfp.write(str(self))
-            
-            os.chddir(directory_for_conversion)
+
+            os.chdir(directory_for_conversion)
             output = subprocess.check_output(["latexmk", "-pdf", latex_file_path],
-                                                stderr=subprocess.STDOUT)
+                                             stderr=subprocess.STDOUT)
             if clean_output_directory:
                 subprocess.Popen(["latexmk", "-c", directory_for_conversion])
             subprocess.Popen(["rm", latex_file_path])
-        except (OSError, ValueError)  as exc:
+        except (OSError, ValueError) as exc:
             raise RuntimeError
         except Exception as exc:
             print(exc)
         finally:
             if DEBUG:
                 print(output.decode())
-
